17884f2b7af4fa2e119a26a9dc5c41c5ecad6a60
App Academy Student <aa@Apps-Mac-mini.local>
2019-01-28 17:44:39 -0500

    initial import

---
+++ src/initial-data.js
@@ -0,0 +1,19 @@
+const initialData = {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'take out the garbage' },
+    'task-2': { id: 'task-2', content: 'watch show' },
+    'task-3': { id: 'task-3', content: 'change phone' },
+    'task-4': { id: 'task-4', content: 'cook dinner' },
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+    }
+  },
+  // Facilitates reordering of the columns 
+  columnOrder: ['column-1'],
+};
+
+export default initialData;
